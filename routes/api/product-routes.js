32bd15c4e5fee3059fd51164c7a6e418cc12bb5d
Sherreka <rekadarnb@gmail.com>
2022-05-20 15:32:09 -0400

    added route to get product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,6 +23,15 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(
+    req.params.id, 
+    {include: [{ model: Category }, [{model: Tag}]]})
+  .then((data) => {
+    res.json(data);
+  })
+  .catch ((err) => {
+    res.status(500).json(err);
+  })
 });
 
 // create new product
