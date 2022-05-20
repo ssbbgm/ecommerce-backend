034ba0d6112e89d93607be5a4b8665a2bdfc2b8a
Sherreka <rekadarnb@gmail.com>
2022-05-20 15:58:52 -0400

    completed tag get routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,27 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{ model: Product }],
+  })
+  .then((data) => {
+    res.json(data);
+  })
+  .catch ((err) => {
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {include: [{ model: Product }]})
+  .then((data) => {
+    res.json(data);
+  })
+  .catch ((err) => {
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
